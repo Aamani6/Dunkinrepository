c4d982bc2f9ea38d432e961201383a6e21c17822
Lakshmi <Lakshmi@DESKTOP-46TS987>
2017-10-08 11:06:49 -0500

    kkkgk

---
+++ practiceproject/src/test/java/employee/Table1.java
@@ -0,0 +1,17 @@
+package employee;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Table1 {
+
+	public static void main(String[] args) {
+		
+		//ChromeDriver ch = new ChromeDriver();
+		
+		
+		
+		
+
+	}
+
+}
